7145cfb149fb1537dfda8b5d1deffeac028055cb
Sebastian Bernal <sbernal@andrew.cmu.edu>
2021-04-07 16:36:40 -0400

    Added a lot more comments to better describe the role of certain variables and parameters Did some code refactoring and removed some redundant calculations and variables Committing before I attempt to change code so theres only one instance of regolith_factor in main

--- Power_Conops_constants.m
+++ Power_Conops_constants.m
@@ -1,34 +1,31 @@
-%%
+%% Setting up the time vector
 %each element in time vector represents a 
 %second increment
 time_scale = 60^2;
 time_start = 0;
-time_step  = 1;  
+time_step  = 1; %single-second increments of time
 time_end   = trek_duration*time_scale; %[Hrs]*[36000 sec/Hr] = sec
-occlusion_end_time = 92136;
+occlusion_end_time = 92136; % times in [s] at which the occlusion ends in the simulation
 tolerance  = 1e-2; %used to check if battery state of charge exceeds 100%
 
 time_vector = time_start: time_step: time_end;
 tv_length = length(time_vector);
 
-%% Occlusion power multipliers
-occ_index = 1;
-occ_times       = [7087, 14175, 21262, 28349, 35437, 42524, 49611, ...
-                    56699, 63786, 70873, 77961, 85048, 92135];
-                
-occ_times_max_power = [3544, 10631, 17719 24806, 31893, ...
-                        38981, 46068, ...
-                        53155, 60243 67330 74417, ...
-                        81505, 88592];
+%% Variables relating to power generation during occultation
+%  a particular time during occlusion (25 hour period)
+%  refer to https://docs.google.com/spreadsheets/d/186Un4aNC-d2LEB51xynQXVAxh9xbEgpof89epkbNYiI/edit#gid=0 
+%  for more information on solar panel temperature dependence and other
+%  relevant calculations
+occ_times       = [0:24].*3600; %change power generation 
                     
-occlusion_powers = [65.63, 66.77 69.04, 71.34, 74.22...
-    75.97, 77.72, 83.62, 85.64, 83.62, ...
-    75.97, 70.19 66.20, 68];
+                    %below are the effective power generations that take
+                    %into account solar panel temperature dependence and
+                    %the varying visible fraction of the solar disk
+                    
+occlusion_powers = [66, 64, 62, 59, 55, 52, 49, 46, 42, 41, 39, 37, 35, ...
+                    29, 22, 20, 18, 19, 20, 28, 36, 44, 52, 58, 65, 68];
 
-occ_multipliers_site1 = [1, 0.93, 0.8, 0.69, 0.57, 0.51, ...
-                    0.45, 0.26, 0.21, 0.24, 0.47, 0.74, 0.98, 1];
-
-%% Power consumption and generation for different modes
+%% Power consumption at different operating modes (based on power equipment list)
 extreme_rove_mode = 58;
 nominal_rove_mode = 53; 
 rove_downlink_mode = 58;
@@ -36,38 +33,51 @@ charge_downlink_mode = 39;
 charge_min_mode = 8;
 charge_max_mode = 25;
 
-%% Vectors for different missions phases and rover characteristics
+%% Vectors for different missions phases (
 plan_trek_interval = [0: time_step: plan_duration*time_scale];
 downlink_interval  = [plan_duration: time_step: downlink_duration*time_scale];
-trek_phase1        = [plan_trek_interval, downlink_interval];                                                       
+trek_phase1        = [plan_trek_interval, downlink_interval];    
+
+%% Setting up the max capacity for "energy bucket" 
+%  Setting up the velocity parameter, and vectors used for plotting
 battery_total = 200*3600; %maximum battery energy capacity in W/hrs
 velocity_cm  = 4;
 velocity_m = velocity_cm/100;
 normal_distance = velocity_m;
+
 battery_soc     = zeros(1,tv_length);
 battery_cap     = zeros(1,tv_length);
 distance_travelled = zeros(1,tv_length);
+
 %% Efficiency multipliers to capture panel temperature dependence
+%  Increase load power by up to 30%; exact percentage increase depends on
+%  and closely resembles the temperature change as outlined in the battery 
+%  temperature transient curve (given by the thermal team).
+
+% The datasheet for the LGMJ1 cells highlights an expected decrease in
+% capacity of 30% when the batteries are in the zero-degree Celcius range
 battery_efficiency_multipliers = zeros(1,tv_length);
 
-battery_efficiency_multipliers(1:35000) = linspace(50,60,35000)./100 + 1;
-battery_efficiency_multipliers(35001:45000) = linspace(60,55,10000)./100 + 1;
-battery_efficiency_multipliers(45001:65000) = linspace(55,60,20000)./100 + 1;
-battery_efficiency_multipliers(65001:92136) = linspace(60,55,27136)./100 + 1;
-battery_efficiency_multipliers(92137:tv_length) = 1.3;
+battery_efficiency_multipliers(1:13000) = linspace(14,30,13000)./100 + 1;
+battery_efficiency_multipliers(13001:18000) = linspace(30,20,5000)./100 + 1;
+battery_efficiency_multipliers(18001:21000) = linspace(20,30,3000)./100 + 1;
+battery_efficiency_multipliers(21001:35000) = linspace(30,20,14000)./100 + 1;
+battery_efficiency_multipliers(35001:65000) = linspace(20,30,30000)./100 + 1;
+battery_efficiency_multipliers(65001:80000) = linspace(30,20,15000)./100 + 1;
+battery_efficiency_multipliers(80001:92000) = linspace(20,30,12000)./100 + 1;
+battery_efficiency_multipliers(92001:tv_length) = 1.2;
 
-panel_efficiency_multipliers(1:35000) = linspace(0,20,35000)./100 + 1;
-panel_efficiency_multipliers(35001:45000) = linspace(20,15,10000)./100 + 1;
-panel_efficiency_multipliers(45001:65000) = linspace(15,20,20000)./100 + 1;
-panel_efficiency_multipliers(65001:92136) = linspace(20,15,27136)./100 + 1;
-panel_efficiency_multipliers(92137:tv_length) = 1;
-%% Vectors that capture the regolith power loss (specified by user parameter)
+%% Vectors that capture the regolith power loss 
+%  (specified by user parameter)
 regolith_factors = zeros(1,tv_length);
 
 regolith_factors(1:occlusion_end_time) = 1 - input_regolith_factor;
-regolith_factors(occlusion_end_time+1:tv_length) = linspace((1-input_regolith_factor), (1-input_regolith_factor+(regolith_factor_delta)), (tv_length-occlusion_end_time));
-%%
+regolith_factors(occlusion_end_time+1:tv_length) = ...
+            linspace((1-input_regolith_factor), ...
+                    (1-input_regolith_factor+(regolith_factor_delta)), ...
+                    (tv_length-occlusion_end_time));
 
+%%
 azimuth_angle = zeros(1, tv_length); %in degrees
 
 %populating azimuth_angle first since
--- Power_Conops_main.m
+++ Power_Conops_main.m
@@ -1,8 +1,10 @@
 %% Populating crater and rock vectors according to user parameters
 %  Crater and rock characteristics determined by the Rock and
-%  CraterDistribution files
+%  CraterDistribution files in this directory
+
 if (enable_rocks)
-    rock_findings = randi([occlusion_end_time, length(time_vector)], 1, length(rockAvoidances)*2);
+    rock_findings = randi([occlusion_end_time, ...
+        length(time_vector)], 1, length(rockAvoidances)*2);
     all_avoided_rocks = zeros(1, length(rockAvoidances));
 else
     rock_findings = [];
@@ -10,20 +12,25 @@ else
 end
 
 if (enable_shadows)
-    shadow_findings = randi([occlusion_end_time, length(time_vector)], 1, 50);
+    shadow_findings = randi([occlusion_end_time, ...
+        length(time_vector)], 1, 50);
 else
     shadow_findings = [];
 end
 
 if (enable_craters)
-    crater_findings = randi([occlusion_end_time, length(time_vector)], 1, length(craterAvoidances)*2);
+    crater_findings = randi([occlusion_end_time, ...
+        length(time_vector)], 1, length(craterAvoidances)*2);
     all_avoided_craters = zeros(1, length(craterAvoidances));
 else
     crater_findings = [];
     all_avoided_craters = [];
 end
 
-%%
+%% Variables to index into rock and crater vectors
+%  Temporary variables to store information about a specific entry in those
+%  vectors as well.
+
 rock_find_index = 1;
 rock_turn_energy = 0;
 rock_straight_distance = 0;
@@ -34,23 +41,26 @@ crater_turn_energy = 0;
 crater_straight_distance = 0;
 crater_turn_time = 0;
 
+% flags/booleans used for control flow logic
 is_charging = false;
 is_avoiding_rock = false;
 is_avoiding_crater = false;
 in_shadow    = false;
 changed_direction = false;
 
-change_power_multiplier = false;
-change_max_power = false;
+%indexes and flags to change the current input power
+change_curr_power = false;
 occ_index = 1;
-occ_max_power_index = 1;
 
+% variables to keep track of avoidance and shadow times
 time_avoiding_rock = 0; %[secs]
 time_avoiding_crater = 0; %[secs]
 time_in_shadow = 0; %[secs]
-distance_covered = 0;
+distance_covered = 0; %[m]
 direction_change_time = 0;
-backAtLander_time = 0;
+backAtLander_time = 0; %[s]
+
+%%
 
 %{
 Factor by which the distance travelled toward target decreases 
@@ -64,59 +74,66 @@ Take the ratio of time2/time1
 
 %% Control flow for obstacle avoidance
 soc_under_100 = true; %flag to make sure battery state of charge is under 100%
+                      %if not used then battery state-of-charge will go
+                      %above 100%
 for i = length(trek_phase1)+1:length(time_vector)
     spec_time = time_vector(i);
     rock_found = ismember(spec_time, rock_findings);
     shadow_found = ismember(spec_time, shadow_findings);
     crater_found = ismember(spec_time, crater_findings);
-    change_power_multiplier = ismember(spec_time, occ_times);
-    change_max_power = ismember(spec_time, occ_times_max_power);
-    
-    if (change_power_multiplier && occ_index < length(occ_times))
+    change_curr_power = ismember(spec_time, occ_times);
+       
+    % change the current power generation based on occ_powers vector in the
+    % _constants files in this directory 
+    % (changes every hour during the occultation)
+    if (change_curr_power && occ_index < length(occ_times))
        occ_index = occ_index + 1; 
     end
     
-    if (change_max_power && occ_max_power_index < length(occ_times_max_power))
-       occ_max_power_index = occ_max_power_index + 1; 
-    end
-    
-    if (distance_travelled(i-1) >= 500 && ~is_avoiding_rock && ~is_avoiding_crater)
+    %this control logic is used to change the sign of 
+    %the distance increments being done on the distance vector
+    %i.e distances changes will be negative once max_distance_from_lander
+    %is reached
+    if (distance_travelled(i-1) >= max_distance_from_lander && ~is_avoiding_rock && ~is_avoiding_crater)
        changed_direction = true; 
        direction_change_time = spec_time;
     end
+       
+    %this is accessible by the rest of the cases in the code
+    %regolith_factors(i) encompasses loss in efficiency due to lunar dust
+    occlusion_power_generation = occlusion_powers(occ_index)*regolith_factors(i);
     
-    power_multiplier = occ_multipliers_site1(occ_index);
-    occlusion_power_generation = occlusion_powers(occ_max_power_index)*power_multiplier*regolith_factors(i);
-    %power_fraction is the factor by which max_solar_flux is multiplied (< 1)
- 
     can_avoid_rock = ~is_charging && ~is_avoiding_rock && ~is_avoiding_crater ...
                      && rock_find_index <= length(rockAvoidances);
     %similarly for craters
     can_avoid_crater = ~is_charging && ~is_avoiding_crater && ~is_avoiding_rock ...
                        && crater_find_index <= length(craterAvoidances);
                    
+    %at the 4 hour mark, power generation falls below nominal power consumption during roving
+    if (spec_time < 4*3600) 
+        energy_change = (occlusion_power_generation - nominal_rove_mode);
+        distance_covered = 0;
     
-    if (spec_time < 4*3600)
-        energy_change = (occlusion_power_generation - charge_max_mode*battery_efficiency_multipliers(i));
-        final_power_vector(i) = occlusion_power_generation;
-        distance_covered = 0;
-        
+    % so after the initial four hours, change mode to
+    % occlusion_power_consumption, and consider battery efficiency
     elseif (spec_time < 92136)
-        energy_change = (occlusion_power_generation - occlusion_power_consumption*battery_efficiency_multipliers(i));
-        final_power_vector(i) = occlusion_power_generation;
+        energy_change = (occlusion_power_generation - ...
+                occlusion_power_consumption*battery_efficiency_multipliers(i));
         distance_covered = 0;
-        
+    
+    % if the following conditionals are reached, rover is in roving mode
     elseif (battery_soc(i-1) < start_charge_soc && ~is_charging)
         is_charging = true;
-        energy_change = (roving_power_generation*angle_offset(i)*regolith_factors(i) - charge_min_mode*battery_efficiency_multipliers(i));
+        energy_change = (roving_power_generation*angle_offset(i)*regolith_factors(i) ...
+                        - charge_min_mode*battery_efficiency_multipliers(i));
         distance_covered = 0;
     
     elseif (is_charging)
-        if (battery_soc(i-1) >= end_charge_soc)
+        if (battery_soc(i-1) >= end_charge_soc) %exit charging_mode
             is_charging = false;
             energy_change = (roving_power_generation*angle_offset(i)*regolith_factors(i) - nominal_rove_mode*battery_efficiency_multipliers(i));
             distance_covered = normal_distance;
-        else
+        else  %continue charging mode until state-of-charge reaches user specified end_charge_soc parameter
             energy_change = (roving_power_generation*angle_offset(i)*regolith_factors(i) - charge_min_mode*battery_efficiency_multipliers(i));
             distance_covered = 0;
         end
